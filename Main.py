350fd91a6ca3d343e19b33ea14a594ba6dbec66e
Kietenergy <68189171+Kietenergy@users.noreply.github.com>
2020-07-14 20:38:06 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,15 +1,20 @@
 #only works when side is more than 4 (5 is minimum)
 import math
-import numpy
 from fractions import Fraction
 side = input("Number of sides? ")
 length = input("Length of the side? ")
-A = int(length) ** 2
+def power(value):
+    new_value = value ** 2
+    return new_value
+A = power(int(length))
 B = int(A) * int(side)
 C = 180/int(side)
-D = math.tan(math.radians(C))
+def trig(value):
+    new_value = math.tan(math.radians(value))
+    return new_value
+D = trig(C)
 E = D * 4
 F = B/E
 print("The area is " + str(F))
 
-#All was made by Keith
+#made by "Keith"
